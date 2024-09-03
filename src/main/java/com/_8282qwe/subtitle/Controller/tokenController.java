e2df25f71a89c0435358ea33d0d76e5712ae642b
jangwoolee <8282qwe@gmail.com>
2024-09-03 12:05:08 +0900

    init commit

--- src/main/java/com/_8282qwe/subtitle/Controller/tokenController.java
+++ src/main/java/com/_8282qwe/subtitle/Controller/tokenController.java
@@ -13,9 +13,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class tokenController {
     private final RecordService recordService;
 
